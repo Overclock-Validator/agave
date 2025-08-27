24133b6ab4016db6bda466dc566efbba03930e41
Rupansh Sekar <rupanshsekar@hotmail.com>
2025-08-27 18:44:52 +0530

    chore(ledger): make coding shred header public

--- ledger/src/shred.rs
+++ ledger/src/shred.rs
@@ -91,7 +91,7 @@ mod payload;
 mod shred_code;
 pub(crate) mod shred_data;
 mod stats;
-mod traits;
+pub mod traits;
 pub mod wire;
 
 // Alias for shred::wire::* for the old code.
@@ -273,10 +273,10 @@ struct DataShredHeader {
 
 /// The coding shred header has FEC information
 #[derive(Clone, Copy, Debug, PartialEq, Eq, SchemaRead, SchemaWrite)]
-struct CodingShredHeader {
-    num_data_shreds: u16,
-    num_coding_shreds: u16,
-    position: u16, // [0..num_coding_shreds)
+pub struct CodingShredHeader {
+    pub num_data_shreds: u16,
+    pub num_coding_shreds: u16,
+    pub position: u16, // [0..num_coding_shreds)
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
