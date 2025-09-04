df235a77e74dc5b2e4a77f15c6165cc18ef52e0d
rupansh <rupanshsekar@hotmail.com>
2025-09-04 19:47:11 +0530

    chore(core/src/repair/serve_repair): mark RepairResponse as public

--- core/src/repair/serve_repair.rs
+++ core/src/repair/serve_repair.rs
@@ -547,7 +547,7 @@ fn is_well_formed_repair_request(packet: &PacketRef, stats: &mut ServeRepairStat
 }
 
 #[derive(Debug, Deserialize, Serialize, SchemaRead, SchemaWrite)]
-pub(crate) enum RepairResponse {
+pub enum RepairResponse {
     Ping(Ping),
 }
 
